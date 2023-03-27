3de5d9d29bd0a253b27b647f2297c94fd2e977a0
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2023-03-27 11:30:27 +0200

    initial commmit

---
+++ getNch.go
@@ -0,0 +1,46 @@
+// test program for namecheap sandbox
+// author: prr azulsoftware
+// date: 26 Mar 2023
+// copyright 2023 prr, azulsoftware
+//
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "time"
+	"log"
+	"context"
+)
+
+func main() {
+
+	getStr := "https://api.sandbox.namecheap.com/xml.response?ApiUser=azulTest&ApiKey=0e5d605ad8cf45fc8fa45e4c529c0448&UserName=azulTest&ClientIp=89.116.30.49&Command=namecheap.domains.check&DomainList=azulTest.com"
+	req, err := http.NewRequest("GET", getStr, nil)
+	if err != nil {
+		log.Fatalf("Req: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(req.Context(), 2*time.Second)
+	defer cancel()
+
+	req = req.WithContext(ctx)
+
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Do: %v", err)
+	}
+
+	fmt.Printf("Response Status Code: %s\n", resp.Status)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Read: %v", err)
+	}
+
+    fmt.Printf("Body:\n %s\n", body)
+
+}
+
